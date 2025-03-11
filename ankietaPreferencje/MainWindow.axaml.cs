f2b8cc8d3ee1c5335ae62ad11786d42a6d988e03
szymonH <szymonH4679@gmail.com>
2025-03-11 16:00:37 +0100

    Added checking personal info

--- ankietaPreferencje/MainWindow.axaml.cs
+++ ankietaPreferencje/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace ankietaPreferencje;
 
@@ -7,5 +8,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+    }
+
+    private void CheckbuttonClick(object? sender, RoutedEventArgs e)
+    {
+        var isInputName = string.IsNullOrEmpty(InputNameTextBox.Text);
+
+        if (!isInputName)
+        {
+            WarningTextBlock.Text = null;
+            var name = InputNameTextBox.Text;
+        }
+        else
+        {
+            WarningTextBlock.Text = "Nie podano imienia!";
+        }
+    
+
+    }
+    private void SendButtonClick(object? sender, RoutedEventArgs e)
+    {
+        
     }
 }
