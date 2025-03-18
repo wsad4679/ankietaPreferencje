0192b4f5a463259878609482c355231b0addc741
szymonH <szymonH4679@gmail.com>
2025-03-18 15:06:40 +0100

    Finished backend

--- ankietaPreferencje/MainWindow.axaml.cs
+++ ankietaPreferencje/MainWindow.axaml.cs
@@ -11,24 +11,48 @@ public partial class MainWindow : Window
         
     }
 
-    private void CheckbuttonClick(object? sender, RoutedEventArgs e)
+    private void CheckIsInput()
     {
         var isInputName = string.IsNullOrEmpty(InputNameTextBox.Text);
 
-        if (!isInputName)
-        {
-            WarningTextBlock.Text = null;
-            var name = InputNameTextBox.Text;
-        }
-        else
+        if (isInputName)
         {
             WarningTextBlock.Text = "Nie podano imienia!";
         }
-    
-
+        else
+        {
+            WarningTextBlock.Text = null;
+        }
+        
+        
     }
+    private void CheckbuttonClick(object? sender, RoutedEventArgs e)
+    {
+        CheckIsInput();
+    }
+    
     private void SendButtonClick(object? sender, RoutedEventArgs e)
     {
-        
+        CheckIsInput();
+        if (WarningTextBlock.Text == "Nie podano imienia!")
+        {
+            SummaryTextBlock.Text = "Wypełnij wszystkie dane";
+        }
+
+        else
+        {
+            var name = InputNameTextBox.Text;
+            var hobby = (Hobby.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano";
+
+            var firstBox = FirstCheckBox.IsChecked == true ? 1 : 0;
+            var secondBox = SecondCheckBox.IsChecked == true ? 1 : 0;
+            var thirdBox = ThirdCheckBox.IsChecked == true ? 1 : 0;
+            var fourthBox = FourthCheckBox.IsChecked == true ? 1 : 0;
+
+            var sumCheckBox = firstBox + secondBox + thirdBox + fourthBox;
+
+            SummaryTextBlock.Text = $"Imię: {name} \n Hobby: {hobby} \n Liczba tak: {sumCheckBox}";
+        }
+
     }
 }
